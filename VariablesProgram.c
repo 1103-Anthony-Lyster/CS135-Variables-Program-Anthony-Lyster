9d058024fc26b9ec9ba709b66301fac51c2caacf
1103-Anthony-Lyster <alyster@nevada.unr.edu>
2024-02-01 22:47:11 +0000

    Variables Project 1st commit

---
+++ VariablesProgram.c
@@ -0,0 +1,14 @@
+//Author: Anthony Lyster
+//Date: 2/1/2024
+//Purpose: Variables Program
+
+#include <stdio.h>
+int main() {
+    char test1;
+        test1 = B;
+    char test2;
+        test2 = A;
+    char test3;
+        test 3;
+    printf("%c\n", <test1>, "%c\n", <test2>, "%c\n", <test3>);  
+}
